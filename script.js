8373a77ade2ed67b3539306b6b1a0269c75792b4
Jeff Shapiro <MacArthur1@MacArthur-Jeffs-MacBook-Pro.local>
2015-05-01 03:11:55 -0400

    Folder Script Backup

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+
+
 var main = function() {
+  
   $('.nav li').click(function() {
+    
     var category = $(this).attr('class');
+		$('.nav li').removeClass('active');
+		$('.thumbnail').removeClass('selected');
+		$(this).addClass('active');
+    
+    if (category==='nav-consumer') {
+    	console.log('nav-consumer clicked');
+    		$('.consumer').addClass('selected');
+    }else if(category==='nav-mobile') {
+    	console.log('nav-mobile clicked');
+    		$('.mobile').addClass('selected');
+    }else if(category==="nav-commerce") {
+    	console.log('nav-commerce clicked');
+    		$('.commerce').addClass('selected');
+    }else if(category==="nav-enterprise") {
+    	console.log('nav-enterprise clicked');
+    		$('.enterprise').addClass('selected');
+    }
 
-    $('.nav li').removeClass('active');
-    $(this).addClass('active');
-
-    
-    
-    
   });
+
 };
  
 $(document).ready(main);
